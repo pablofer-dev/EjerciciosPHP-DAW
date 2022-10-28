ea665dd7298617a8ee55f1f70216165187b767bc
Pablo <pfl0002@alu.medac.es>
2022-10-28 16:25:59 +0200

    php unit

--- Tema6/PHPunit/src/DniEjemplo.php
+++ Tema6/PHPunit/src/DniEjemplo.php
@@ -1,53 +1,72 @@
 <?php
 
-    declare(strict_types=1);
+/* Una característica de PHP 7 que obliga a que el tipo de los parámetros sea el mismo que el tipo
+declarado en la función. */
 
+declare(strict_types=1);
+class DniEjemplo
+{
 
-    class DniEjemplo {
-
-        private $dni;
-        private const longitud_dni = 9;
-        private const letra_dni = 'TRWAGMYFPDXBNJZSQVHLCKE';
-
-        public function __construct(string $dni)
-        {
-        //Controlamos Longitud
-            if (strlen($dni) > self::longitud_dni) {
-                throw new LengthException('Debe introducir 9 carácteres, no más');
-            } elseif (strlen($dni) < self::longitud_dni) {
-                throw new LengthException('Debe introducir 9 carácteres, no menos');
-        //Controlamos que el DNI termine por una letra
-            } elseif (preg_match('/\d$/', $dni)) {
-                throw new DomainException('El DNI debe terminar por una letra');
-        //Controlamos que el DNI no termine por una letra como [I, O, U, Ñ]
-            } elseif (preg_match('/[UIOÑ]$/u', $dni)) {
-                throw new DomainException('El DNI no debe terminar por la letra [I, O, U, Ñ]');
-        //Controlamos que el DNI no contenga letra intermedias
-            } elseif (!preg_match('/\d{7,7}.$/', $dni)) {
-                throw new DomainException('Un DNI no puede tener letras intermedias');
-        //Controlo que el DNI no contenga una letra inicial que no sea diferente de XYZ
-            } elseif (! preg_match('/^[XYZ0-9]/', $dni)) {
-                throw new DomainException('En caso de un NIF debe iniciar por las letras XYZ');
-        //Controlo que la letra del DNI sea la correcta 
-            } elseif (!($this -> validarDNI($dni))) {
-                throw new InvalidArgumentException('La letra del dni introducido no es correcta');
-            }   
-        }
-        
-        
-        public function validarDNI(string $dni)
-        {
-            $letter = substr($dni, -1);
-            $numbers = substr($dni, 0, -1);
-
-            if (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numbers%23, 1) == $letter && strlen($letter) == 1 && strlen ($numbers) == 8 ){
-              return true;
-            }
-            return false;
-        }
-
-        public function toString() {
-            return $this -> dni;
+    private $dni;
+    /**
+     * Comprueba que la longitud de la cadena sea inferior a 9 caracteres, que la longitud de la cadena
+     * sea inferior a 9 caracteres, que el DNI termine en letra, que el DNI no termine en letras I, O,
+     * U o Ñ , que el DNI no contenga letras intermedias, que el DNI no empiece por una letra distinta
+     * a XYZ y que el DNI sea válido
+     * 
+     * @param dni El número de DNI.
+     */
+    public function __construct(string $dni)
+    {
+        /* Comprobando que la longitud de la cadena es inferior a 9 caracteres. */
+        if (strlen($dni) > 9) {
+            throw new LengthException('Debe introducir 9 carácteres, no más');
+            /* Comprobando que la longitud de la cadena es inferior a 9 caracteres. */
+        } elseif (strlen($dni) < 9) {
+            throw new LengthException('Debe introducir 9 carácteres, no menos');
+            /* Comprobando que el DNI termina en letra. */
+        } elseif (preg_match('/\d$/', $dni)) {
+            throw new DomainException('El DNI debe terminar por una letra');
+            /* Comprobando que el DNI no termina en las letras I, O, U o Ñ. */
+        } elseif (preg_match('/[UIOÑ]$/u', $dni)) {
+            throw new DomainException('El DNI no debe terminar por la letra [I, O, U, Ñ]');
+            /* Comprobando que el DNI no contiene letras intermedias. */
+        } elseif (!preg_match('/\d{7,7}.$/', $dni)) {
+            throw new DomainException('Un DNI no puede tener letras intermedias');
+            /* Comprobando que el DNI no empieza por una letra diferente a XYZ. */
+        } elseif (!preg_match('/^[XYZ0-9]/', $dni)) {
+            throw new DomainException('En caso de un NIF debe iniciar por las letras XYZ');
+            /* Comprobando si el DNI es válido. */
+        } elseif (!($this->comprobarLetraDni($dni))) {
+            throw new InvalidArgumentException('La letra del dni introducido no es correcta');
         }
     }
-?>
+
+    /**
+     * Valida un DNI (DNI español)
+     * 
+     * @param dni El DNI a validar.
+     * 
+     * @return un valor booleano.
+     */
+    public function comprobarLetraDni(string $dni)
+    {
+        /* Comprobando si el DNI es válido. */
+        $letter = substr($dni, -1);
+        $numbers = substr($dni, 0, -1);
+        if (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numbers % 23, 1) == $letter && strlen($letter) == 1 && strlen($numbers) == 8) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * El método toString() devuelve el valor de la propiedad dni.
+     * 
+     * @return El DNI de la persona.
+     */
+    public function toString()
+    {
+        return $this->dni;
+    }
+}
--- Tema6/PHPunit/tests/DniEjemploTest.php
+++ Tema6/PHPunit/tests/DniEjemploTest.php
@@ -1,58 +1,57 @@
 <?php
-    use PHPUnit\Framework\TestCase;
 
-    final class DniTest extends TestCase{
+use PHPUnit\Framework\TestCase;
 
-        //Controlo que el DNI tenga nueve dígitos
-        public function testDniLongitudMayor()
-            {
-                $this->expectException(LengthException::class);
-                $this->expectExceptionMessage('Debe introducir 9 carácteres, no más');
-                $dni = new DniEjemplo('0123456789');
-            }
-        
-        public function testDniLongitudMenor(): void
-            {
-                $this->expectException(LengthException::class);
-                $this->expectExceptionMessage('Debe introducir 9 carácteres, no menos');
-                $dni = new DniEjemplo('01234567');
-            }
-        //Controlo que el DNI termine por una letra
-        public function testDniAcabaConLetra(): void
-            {
-                $this->expectException(DomainException::class);
-                $this->expectExceptionMessage('El DNI debe terminar por una letra');
-                $dni = new DniEjemplo('012345678');
-            }
-        //Controlo que el DNI termine por una letra que no sea [I, O, U, Ñ]
-        public function testDniAcabaConLetraDiferenteIOUÑ() : void
-            {
-                $this->expectException(DomainException::class);
-                $this->expectExceptionMessage('El DNI no debe terminar por la letra [I, O, U, Ñ]');
-                $dni = new DniEjemplo('01234567I');
-            }
-        //Controlo que el DNI no contenga una letra intermedia
-        public function testDniContieneLetrasEnMedio(): void
-            {
-                $this->expectException(DomainException::class);
-                $this->expectExceptionMessage('Un DNI no puede tener letras intermedias');
-                $dni = new DniEjemplo('012AB567R');
-            }
-        ///Controlo que el DNI no contenga una letra inicial que no sea diferente de XYZ
-        public function testDniAcabaConLetraDiferenteXYZ(): void
-            {
-                $this->expectException(DomainException::class);
-                $this->expectExceptionMessage('En caso de un NIF debe iniciar por las letras XYZ');
-                $dni = new DniEjemplo('A1234567R');
-            }
-        //Controlo que el DNI no contenga su letra correcta
-        public function testDniUltimaLetraCorrecta(): void
-            {
-                $this->expectException(InvalidArgumentException::class);
-                $this->expectExceptionMessage('La letra del dni introducido no es correcta');
-                $dni = new DniEjemplo('00000000S');
-            }
-        //Controlo que el DNI no contenga su letra correcta
-       
-        
+final class DniTest extends TestCase
+{
+
+    //Controlo que el DNI tenga nueve dígitos
+    public function testDniLongitudMayor()
+    {
+        $this->expectException(LengthException::class);
+        $this->expectExceptionMessage('Debe introducir 9 carácteres, no más');
+        $dni = new DniEjemplo('0123456789');
     }
+
+    public function testDniLongitudMenor(): void
+    {
+        $this->expectException(LengthException::class);
+        $this->expectExceptionMessage('Debe introducir 9 carácteres, no menos');
+        $dni = new DniEjemplo('01234567');
+    }
+    //Controlo que el DNI termine por una letra
+    public function testDniAcabaConLetra(): void
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('El DNI debe terminar por una letra');
+        $dni = new DniEjemplo('012345678');
+    }
+    //Controlo que el DNI termine por una letra que no sea [I, O, U, Ñ]
+    public function testDniAcabaConLetraDiferenteIOUÑ(): void
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('El DNI no debe terminar por la letra [I, O, U, Ñ]');
+        $dni = new DniEjemplo('01234567I');
+    }
+    //Controlo que el DNI no contenga una letra intermedia
+    public function testDniContieneLetrasEnMedio(): void
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('Un DNI no puede tener letras intermedias');
+        $dni = new DniEjemplo('012AB567R');
+    }
+    ///Controlo que el DNI no contenga una letra inicial que no sea diferente de XYZ
+    public function testDniAcabaConLetraDiferenteXYZ(): void
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('En caso de un NIF debe iniciar por las letras XYZ');
+        $dni = new DniEjemplo('A1234567R');
+    }
+    //Controlo que el DNI no contenga su letra correcta
+    public function testDniUltimaLetraCorrecta(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('La letra del dni introducido no es correcta');
+        $dni = new DniEjemplo('00000000S');
+    }
+}
